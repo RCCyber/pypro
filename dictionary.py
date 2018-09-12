632ed645daf1e453e149622609de9163d868fc47
RCCyber <8jane7@gmail.com>
2018-09-12 20:21:54 +0300

    4.0

--- dictionary.py
+++ dictionary.py
@@ -1,11 +1,14 @@
 import random
 
 a = random.randint(100, 999)
-src_a = {'key_1': a}
+a1 = str(input("Enter the name of first key: "))
+src_a = {a1: a}
 b = random.randint(100, 999)
-src_b = {'key_2': b}
+b1 = str(input("Enter the name of second key: "))
+src_b = {b1: b}
 c = random.randint(100, 999)
-src_c = {'key_3': c}
+c1 = str(input("Enter the name of third key: "))
+src_c = {c1: c}
 
 
 def summary(src_a, src_b, src_c):
