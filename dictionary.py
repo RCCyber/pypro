8b4996e8969959d836087b1f8c2d28148c59283d
RCCyber <8jane7@gmail.com>
2018-09-12 20:16:09 +0300

    4.0

--- dictionary.py
+++ dictionary.py
@@ -1,8 +1,16 @@
-def summary():
-    src_a = {'key_1': 123}
-    src_b = {'key_2': 489}
-    src_c = {'key_3': 900}
-    print(src_a, src_b, src_c)
-    print("huehue")
+import random
 
-summary()
+a = random.randint(100, 999)
+src_a = {'key_1': a}
+b = random.randint(100, 999)
+src_b = {'key_2': b}
+c = random.randint(100, 999)
+src_c = {'key_3': c}
+
+
+def summary(src_a, src_b, src_c):
+    src_x = {**src_a, **src_b, **src_c}
+    print(src_x)
+
+
+summary(src_a, src_b, src_c)
